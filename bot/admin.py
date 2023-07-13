03370ff3b9c286e367a8662109ff4b40379465ae
Artur Olenberg <olenberg.ai@gmail.com>
2023-07-13 21:32:58 +0600

    add bot

--- bot/admin.py
+++ bot/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from bot.models import TgUser
 
 
-@admin.register(TgUser)
-class TgUserAdmin(admin.ModelAdmin):
-    list_display = ["chat_id", "user"]
-    readonly_fields = ["verification_code"]
-    search_fields = ["chat_id"]
+# @admin.register(TgUser)
+# class TgUserAdmin(admin.ModelAdmin):
+#     list_display = ["chat_id", "user"]
+#     readonly_fields = ["verification_code"]
+#     search_fields = ["chat_id"]
--- bot/models.py
+++ bot/models.py
@@ -4,8 +4,8 @@ from core.models import User
 
 
 class TgUser(models.Model):
-    tg_chat_id = models.BigIntegerField()
-    tg_user_id = models.BigIntegerField(unique=True)
+    tg_chat_id = models.BigIntegerField(null=True)
+    tg_user_id = models.BigIntegerField(unique=True, null=True)
     tg_username = models.CharField(max_length=32, validators=[MinLengthValidator(5)], null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,)
     verification_code = models.CharField(max_length=32, unique=True, null=True, blank=True)
